5c2c7761ad39c316045b8010553e953784cab139
mbodenchak <mbodenchak@outlook.com>
2022-06-04 14:21:18 -0700

    created skeleton placeItem component

--- components/Places/PlacesList.js
+++ components/Places/PlacesList.js
@@ -1,7 +1,16 @@
 import { FlatList } from "react-native";
+import PlaceItem from "./PlaceItem";
 
 function PlacesList({ places }) {
-  return <FlatList data={places} keyExtractor={(item) => item.id} renderItem={}/>;
+  return (
+    <FlatList
+      data={places}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => {
+        <PlaceItem place={item} />;
+      }}
+    />
+  );
 }
 
 export default PlacesList;
