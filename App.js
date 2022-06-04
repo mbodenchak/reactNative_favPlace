833933a819480320aa5faa39a02194be8f07a0f7
mbodenchak <mbodenchak@outlook.com>
2022-06-04 14:33:12 -0700

    simple nav step 1

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AllPlaces from "./screens/AllPlaces";
+import AddPlace from "./screens/AddPlace";
+
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
     <>
       <StatusBar style="dark" />
-      <NavigationContainer></NavigationContainer>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="AllPlaces" component={AllPlaces} />
+          <Stack.Screen name="AddPlace" component={AddPlace} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
