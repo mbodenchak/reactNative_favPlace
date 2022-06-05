7c7956917a6bd0f91cf265fb48a221f42c07bfa0
mbodenchak <mbodenchak@outlook.com>
2022-06-04 17:06:53 -0700

    added camera functionality

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import AllPlaces from "./screens/AllPlaces";
 import AddPlace from "./screens/AddPlace";
 import IconButton from "./components/UI/IconButton";
-
+import { Colors } from "./constants/colors";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
@@ -12,12 +12,19 @@ export default function App() {
     <>
       <StatusBar style="dark" />
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: Colors.primary500 },
+            headerTintColor: Colors.gray700,
+            contentStyle: { backgroundColor: Colors.gray700 },
+          }}
+        >
           <Stack.Screen
             name="AllPlaces"
             component={AllPlaces}
             //turn options into a function in order to get the navigation prop
             options={({ navigation }) => ({
+              title: "Your Places",
               headerRight: ({ tintColor }) => (
                 <IconButton
                   icon="add"
@@ -28,7 +35,13 @@ export default function App() {
               ),
             })}
           />
-          <Stack.Screen name="AddPlace" component={AddPlace} />
+          <Stack.Screen
+            name="AddPlace"
+            component={AddPlace}
+            options={{
+              title: "Add A New Place",
+            }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
